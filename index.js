3f7918fb7aeb72446061a6cd0babf62c9c8b311c
unknown <levitclark1995@gmail.com>
2018-07-31 08:59:37 -0500

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'levi' });
+  res.send({ hi: 'levi clark' });
 });
 
 const PORT = process.env.PORT || 5000;
